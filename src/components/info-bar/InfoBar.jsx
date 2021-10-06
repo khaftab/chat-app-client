14d210478876b5434b122a17ffd8097639ebaef7
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2021-10-06 20:54:13 +0530

    fix alt tag

--- src/components/info-bar/InfoBar.jsx
+++ src/components/info-bar/InfoBar.jsx
@@ -7,11 +7,11 @@ function InfoBar({ room }) {
     return (
         <div className="infoBar">
             <div className="leftInnerContainer">
-                <img src={onlineIcon} alt="Image" className='on line icon' />
+                <img src={onlineIcon} alt="Online Icon" className='on line icon' />
                 <h3>{room}</h3>
             </div>
             <div className="rightInnerContainer">
-                <a href="/"> <img src={closeIcon} alt="close image" /></a>
+                <a href="/"> <img src={closeIcon} alt="Close Icon" /></a>
             </div>
         </div>
     )
